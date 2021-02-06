61c2454efbd59959bf1d19d1ad2a9c8ab4dea6b8
Soji Antony <soji.antony@olacabs.com>
2021-02-06 18:43:02 +0530

    Added Question Section

--- python/stack/merge_intervals.py
+++ python/stack/merge_intervals.py
@@ -3,6 +3,12 @@ import sys
 logging.basicConfig(level=logging.INFO,stream=sys.stdout,format="%(message)s")
 logger = logging.getLogger()
 
+"""
+Q: Merge Overlapping Intervals
+For example, let the given set of intervals be {{1,3}, {2,4}, {5,7}, {6,8}}. The intervals {1,3} and {2,4} overlap with each other, 
+so they should be merged and become {1, 4}. Similarly, {5, 7} and {6, 8} should be merged and become {5, 8} 
+"""
+
 
 class Stack:
     def __init__(self):
