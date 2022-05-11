2e577883cf3a2374910d31a858daf1b5cc929e63
Soji Antony <soji.antony@razorpay.com>
2022-05-11 17:23:03 +0530

    Added optimization in bubble sort to avoid unwanted passes

--- go/sorting/bubble_sort/sort.go
+++ go/sorting/bubble_sort/sort.go
@@ -20,10 +20,15 @@ func init() {
 func BubbleSort(ar []int) []int {
 	l := len(ar)
 	for i := 0; i < l-1; i++ {
+		sorted := false
 		for j := 0; j < (l - 1 - i); j++ {
 			if ar[j] > ar[j+1] {
 				ar[j], ar[j+1] = ar[j+1], ar[j]
+				sorted = true
 			}
+		}
+		if sorted == false {
+			break
 		}
 	}
 	return ar
